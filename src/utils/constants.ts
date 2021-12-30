124329e7585364f4444fe4de4be8a3c21c8515d8
Gabriel Siedler <gabrielsiedler@gmail.com>
2021-12-30 12:19:28 -0300

    adds timeout constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 export default {
   WAIT: 30 * 1000,
   LONGWAIT: 15 * 60 * 1000,
+  TIMEOUT: 60 * 1000,
 }
--- src/worker.ts
+++ src/worker.ts
@@ -13,6 +13,8 @@ export const start = async (browser) => {
     for (let i = 0; i < runners.length; i += 1) {
       const tab = await browser.newPage()
 
+      await tab.setDefaultNavigationTimeout(constants.TIMEOUT)
+
       await tab.setUserAgent(
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
       )
