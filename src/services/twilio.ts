bc21731ff02bfe0468155a637d2f42ecbb921e6b
Gabriel Siedler <gabrielsiedler@gmail.com>
2021-12-31 14:44:11 -0300

    twilio error catching

--- src/services/twilio.ts
+++ src/services/twilio.ts
@@ -3,9 +3,13 @@ import { twilioClient } from '../setup/twilio'
 export const sendWhatsappMessage = async (source, { id, href, title }) => {
   const message = `${href}\n\nNova casa em ${source}.\n\n${id} - ${title}`
 
-  await twilioClient.messages.create({
-    from: 'whatsapp:+14155238886',
-    to: `whatsapp:${process.env.TWILIO_TARGET_NUMBER}`,
-    body: message,
-  })
+  try {
+    await twilioClient.messages.create({
+      from: 'whatsapp:+14155238886',
+      to: `whatsapp:${process.env.TWILIO_TARGET_NUMBER}`,
+      body: message,
+    })
+  } catch (e) {
+    console.error('Could not send message to Twilio!', e)
+  }
 }
